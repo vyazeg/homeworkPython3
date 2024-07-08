86a9af99324705d4b55aae38d59d286b006ecd5c
Egor Viazkov <vyazkovegor@gmail.com>
2024-07-08 19:21:50 +0300

    Исправил селектор на правильный

--- conftest.py
+++ conftest.py
@@ -4,8 +4,8 @@ import pytest
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_start():
-    browser.open('https://google.com')
     browser.config.window_height = 1080
     browser.config.window_width = 1920
+    browser.config.base_url = 'https://google.com'
     yield
-    browser.close()
+    browser.quit()
--- test_google.py
+++ test_google.py
@@ -2,11 +2,12 @@ from selene import browser, have, be
 
 
 def test_google():
+    browser.open('')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_google_no_reusult():
+def test_google_no_result():
+    browser.open('')
     browser.element('[name="q"]').should(be.blank).type('asdfafvadvsdfvaasccadsasdc2333fdsfsdf').press_enter()
-    browser.element('#botstuff').should(have.text('По запросу asdfafvadvsdfvaasccadsasdc2333fdsfsdf ничего не найдено'))
-
+    browser.element('.card-section').should(have.text('По запросу asdfafvadvsdfvaasccadsasdc2333fdsfsdf ничего не найдено'))
