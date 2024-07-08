eea8011bac0b2b997d8909d7d190c6ae118d6691
Egor Viazkov <vyazkovegor@gmail.com>
2024-07-08 19:03:49 +0300

    Добавил фикстуру

--- test_google.py
+++ test_google.py
@@ -1,3 +1,12 @@
-def test_google(browser_start):
-    pass
+from selene import browser, have, be
+
+
+def test_google():
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_google_no_reusult():
+    browser.element('[name="q"]').should(be.blank).type('asdfafvadvsdfvaasccadsasdc2333fdsfsdf').press_enter()
+    browser.element('#botstuff').should(have.text('По запросу asdfafvadvsdfvaasccadsasdc2333fdsfsdf ничего не найдено'))
 
